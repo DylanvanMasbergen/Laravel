bb85b36fc7e4214c9cb38cfe3b9ebf74c45935a2
Dylan van Masbergen <99039655@mydavinci.nl>
2018-10-01 09:47:51 +0200

    Added ProductController, added Product Factory and Migration.

---
+++ database/migrations/2018_09_26_114425_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('Product_id');
+            $table->string('Product_name');
+            $table->string('Product_description');
+            $table->string('Product_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
